50cb80b8adbea5362140eba76d073782be1d5de4
Ankan Basu <ankanbasu@mac.attlocal.net>
2021-05-07 16:57:07 -0400

    first commit

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+message = "Hello World!"
+print(message)
+
+message = "test"
+
+print(message.title())
+print(message.capitalize())
+print(message.swapcase())
+
+first_name = "Ava"
+last_name = "lovelace"
+
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+print(f"Hello, {first_name.title()}, {last_name.title()}")
+
+print("hello\n World")
